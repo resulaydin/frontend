1a2467ac8035f8fa986b235883638eabb22addd0
resul aydın <bm.resulaydin@gmail.com>
2023-06-11 15:57:43 +0300

    Revert "login page and register page with css files has created"
    This reverts commit 96d2172648ce9d6afe3c451ef9b5b71244d42da3.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import "./style/main.css";
-import UserSignUpPage from "./pages/UserSignUpPage";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/register" exact element={<Register />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/register" exact element={<Register />} />
-        <Route path="/user-signup" exact element={<UserSignUpPage />} />
-      </Routes>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
