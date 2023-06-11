a8cf4799745886d993ee4979ed58e1c36707db6b
resul aydın <bm.resulaydin@gmail.com>
2023-06-11 16:12:24 +0300

    set to be udemy courses practice only

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import "./style/main.css";
 import UserSignUpPage from "./pages/UserSignUpPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/register" exact element={<Register />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/register" exact element={<Register />} />
-        <Route path="/user-signup" exact element={<UserSignUpPage />} />
+        <Route path="/" exact element={<UserSignUpPage />} />
       </Routes>
     </div>
   );
